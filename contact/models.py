93b20cdc3466c23f460d591a0665bdfb12d45984
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-21 13:58:45 -0300

    'adicionando-campos-nas-models'

--- contact/models.py
+++ contact/models.py
@@ -9,6 +9,8 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
