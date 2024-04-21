d36a0f507e5e853e3f900b86f30922c6eab99c41
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-21 14:17:39 -0300

    'model_categoria-e-chave_estrangeira'

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Contact(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30, blank=True)
@@ -11,6 +20,11 @@ class Contact(models.Model):
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(blank=True, upload_to='pictures/%Y/%m/')
+    category = models.ForeignKey(
+        Category, 
+        on_delete=models.SET_NULL,
+        blank=True, null=True
+    )
 
     def __str__(self) -> str:
         return f'{self.first_name} {self.last_name}'
