e2313579e4c0c74c7e80b375ad807809d6200950
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-21 12:45:22 -0300

    'configurando-area-adm'

--- contact/admin.py
+++ contact/admin.py
@@ -4,4 +4,11 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    pass
+    list_display  = 'id', 'first_name', 'last_name', 'phone',
+    ordering = '-id',
+    search_fields = 'id', 'first_name', 'last_name',
+    #list_filter = 'created_date',
+    list_per_page = 10
+    list_max_show_all = 200
+    list_editable = 'first_name', 'last_name',
+    list_display_links = 'id', 'phone',
