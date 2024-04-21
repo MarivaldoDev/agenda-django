5ef82170242f1c9efcc29ae3cd482adc877e6e19
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-20 23:38:38 -0300

    'registrando_model_na_area_adm'

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from contact import models
 
 # Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    pass
--- contact/models.py
+++ contact/models.py
@@ -9,4 +9,6 @@ class Contact(models.Model):
     email = models.EmailField(max_length=254)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
-    
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
