25d918e438b48d230e52d5a5d14079734be6325c
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-25 20:24:45 -0300

    'criando-filtros'

--- contact/admin.py
+++ contact/admin.py
@@ -4,13 +4,13 @@ from contact import models
 # Register your models here.
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display  = 'id', 'first_name', 'last_name', 'phone',
+    list_display  = 'id', 'first_name', 'last_name', 'phone', 'show',
     ordering = '-id',
     search_fields = 'id', 'first_name', 'last_name',
     #list_filter = 'created_date',
     list_per_page = 10
     list_max_show_all = 200
-    list_editable = 'first_name', 'last_name',
+    list_editable = 'first_name', 'last_name', 'show',
     list_display_links = 'id', 'phone',
 
 
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -3,7 +3,9 @@ from contact.models import Contact
 
 # Create your views here.
 def index(request):
-    contacts = Contact.objects.all()
+    contacts = Contact.objects\
+        .filter(show=True)\
+        .order_by('-id')[:10]
 
     context = {
         'contacts': contacts,
