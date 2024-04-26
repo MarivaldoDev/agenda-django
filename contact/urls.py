db580779db9697c0d884b6e8b4e352f9c05645fd
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-25 21:00:36 -0300

    'criando-contato-unico'

--- contact/urls.py
+++ contact/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from contact import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('<int:contact_id>/', views.contact, name='contact')
 ]
--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from contact.models import Contact
 
 # Create your views here.
@@ -12,3 +12,14 @@ def index(request):
     }
 
     return render(request, 'contact/index.html', context)
+
+def contact(request, contact_id):
+    # single_contact = Contact.objects.get(pk=contact_id)
+    single_contact = get_object_or_404(
+        Contact, pk=contact_id, show=True
+    )
+    context = {
+        'contact': single_contact,
+    }
+
+    return render(request, 'contact/contact.html', context)
