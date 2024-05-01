e71887eb62c5017e3880b6dc663622abac327534
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-30 22:09:33 -0300

    'finalizando_forms...'

--- contact/forms.py
+++ contact/forms.py
@@ -24,11 +24,22 @@ class ContactForm(forms.ModelForm):
 
     class Meta:
         model = Contact
-        fields = ("first_name", 'last_name', 'phone',)
+        fields = (
+            'first_name', 'last_name', 'phone',
+            'email', 'description', 'category',
+        )
 
     def clean(self):
-        # cleaned_data = self.cleaned_data
+        cleaned_data = self.cleaned_data
+        first_name = cleaned_data.get('first_name')
+        last_name = cleaned_data.get('last_name')
 
-        self.add_error('first_name', ValidationError('Errooooooo', code='invalid'))
+        if first_name == last_name:
+            msg = ValidationError(
+                'Primeiro nome não pode ser igual ao segundo',
+                code='invalid'
+            )
+            self.add_error('first_name', msg)
+            self.add_error('last_name', msg)
 
         return super().clean()
