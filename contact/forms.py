b122d22e80c52dad43a9bd4befa4da3d556b73a2
MarivaldoDev <pedromarivaldo10@gmail.com>
2024-04-30 21:22:48 -0300

    'ainda-no-formulário'

--- contact/forms.py
+++ contact/forms.py
@@ -4,6 +4,24 @@ from contact.models import Contact
 
 
 class ContactForm(forms.ModelForm):
+    first_name = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'classe-a classe-b',
+                'placeholder': 'Seu nome...',
+            }
+        ),
+        help_text='Texto de ajuda',
+    )
+    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # self.fields['first_name'].widget.attrs.update({
+        #     'class': 'classe-a classe-b',
+        #     'placeholder': 'Seu nome...'
+        # })
+
     class Meta:
         model = Contact
         fields = ("first_name", 'last_name', 'phone',)
